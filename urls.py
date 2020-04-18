5f7b35898ad8ce109d16a7ed8e9c6808274f78f5
Shubhangi2999 <30768563+Shubhangi2999@users.noreply.github.com>
2020-04-18 22:25:09 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^api/partners/$', views.partners_list),
-    url(r'^api/partners/details/$', views.partners_detail),
-]

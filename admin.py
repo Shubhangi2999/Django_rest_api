d31ae3f56d358742f6d716cf37e727f49c5a7603
Shubhangi2999 <30768563+Shubhangi2999@users.noreply.github.com>
2020-04-18 22:22:56 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Partner
-# Register your models here.
-
-class PartnerAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(Partner,PartnerAdmin)
